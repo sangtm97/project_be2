d6af4d6f9dcea004ab0898379b6a82d2da9a4a8e
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2022-06-08 20:07:44 +0700

    update module2

--- resources/views/module-2.blade.php
+++ resources/views/module-2.blade.php
@@ -1,62 +1,47 @@
 <div class="module-2">
-    <div class="slider">
+    <div class="container-fliud">
         <div class="slides">
-            <input type="radio" name="radio-btn" id="radio1">
-            <input type="radio" name="radio-btn" id="radio2">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner" style="width:100%">
+                    <div class="carousel-item active">
+                        <div class="heading">
+                            <div class="title">
+                                <h1>Comfort and <br>Connection</h1>
+                            </div>
+                            <div class="description">
+                                <p>The recliner has gotten a stylish makeover. <br>The newest styles feature clean lines
+                                    &amp; sophisticated fabrics</p>
+                            </div>
+                            <div class="razzi-slide-button"><a
+                                    href="https://demo4.drfuri.com/razzi9/product/pollock-executive-chair/"
+                                    class="button-link"><span class="button-text razzi-button">Shop Now </span></a>
+                            </div>
+                        </div>
+                        <img src="./2/public/images/Home-v13-Slide-1.png" class="d-block w-100" style="width:100%"
+                            alt="...">
 
-            <div class="slide first">
-                <div class="slide-content">
-                    <div class="slide-Title">
-                        <h1>New Season<br>New Mood</h1>
                     </div>
-                    <div class="slide-description">
-                        <p>Illuminate the inside and outside of your home with <br>our selection of Lighting
-                            solutions</p>
+                    <div class="carousel-item">
+                        <div class="heading">
+                            <div class="title">
+                                <h1>New Season <br>
+                                    New Mood</h1>
+                            </div>
+                            <div class="description">
+                                <p>Illuminate the inside and outside of your home with <br>our selection of Lighting
+                                    solutions</p>
+                            </div>
+                            <div class="razzi-slide-button"><a
+                                    href="https://demo4.drfuri.com/razzi9/product/pollock-executive-chair/"
+                                    class="button-link"><span class="button-text razzi-button">Shop Now </span></a>
+                            </div>
+                        </div>
+                        <img src="./2/public/images/Home-v13-Slide-2.png" class="d-block w-100" style="width:100%"
+                            alt="...">
                     </div>
-                    <div class="button-shopnow">
-                        <button><a href="https://demo4.drfuri.com/razzi9/product/the-globe-pendant-light/"
-                                class="button-link"><span class="button-text razzi-button">Shop Now </span></a>
-                    </div></button>
                 </div>
-                <img src="./public/images/Home-v13-Slide-1.png" alt="">
-
             </div>
-            <div class="slide">
-                <div class="slide-content">
-                    <div class="slide-Title">
-                        <h1>New Season<br>New Mood</h1>
-                    </div>
-                    <div class="slide-description">
-                        <p>Illuminate the inside and outside of your home with <br>our selection of Lighting
-                            solutions</p>
-                    </div>
-                    <div class="button-shopnow">
-                        <button><a href="https://demo4.drfuri.com/razzi9/product/the-globe-pendant-light/"
-                                class="button-link"><span class="button-text razzi-button">Shop Now </span></a>
-                    </div></button>
-                </div>
-                <img src="./public/images/Home-v13-Slide-2.png" alt="">
-            </div>
-
-            <div class="navigation-auto">
-                <div class="auto-btn1"></div>
-                <div class="auto-btn2"></div>
-            </div>
-        </div>
-
-        <div class="navigation-manual">
-            <label for="radio1" class="manual-btn"></label>
-            <label for="radio2" class="manual-btn"></label>
         </div>
     </div>
-    <script type="text/javascript">
-    var counter = 1;
-    setInterval(function() {
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if (counter > 2) {
-            counter = 1;
-        }
-    }, 5000);
-    </script>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
