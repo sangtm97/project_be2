62f4a4314067ad47763ef55ea89cf0ca0135d960
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2022-06-10 00:25:59 +0700

    update form

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -91,5 +91,35 @@
             </ul>
         </nav>
     </div>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Product
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="./add" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>Add New Product</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="./list" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>List Product</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
     <!-- /.sidebar -->
 </aside>
