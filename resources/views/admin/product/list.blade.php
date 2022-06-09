10824c526bb1ee181cb7a55c0eb3d67e2791c2ac
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2022-06-10 00:33:55 +0700

    create delete product

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -5,20 +5,27 @@
         <thead>
             <tr>
                 <th>Id</th>
-                <th>Category name</th>
-                <th>Action</th>
+                <th>Product name</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Image</th>
+                <th>Endow_id</th>
             </tr>
         </thead>
         <tbody>
-        @foreach($categories as $category)
+        @foreach($products as $product)
             <tr>
-                <td>{{$category->id}}</td>
-                <td>{{$category->category_name}}</td>
+                <td>{{$product->id}}</td>
+                <td>{{$product->product_name}}</td>
+                <td>{{$product->product_price}}</td>
+                <td>{{$product->product_description}}</td>
+                <td>{{$product->product_image}}</td>
+                <td>{{$product->endow_id}}</td>
                 <td style="display: flex;gap: 6px">
-                    <a class="btn btn-primary btn-sm" href="{{route('edit', $category->id)}}">
+                    <a class="btn btn-primary btn-sm" href="{{route('edit', $product->id)}}">
                         Edit
                     </a>
-                    <form action="{{route('destroy', $category->id)}}" method="POST">
+                    <form action="{{route('destroy', $product->id)}}" method="POST">
                         @method('DELETE')
                         @csrf
                         <button type="submit" class="btn btn-danger">Delete</button>
