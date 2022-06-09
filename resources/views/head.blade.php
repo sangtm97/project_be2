278ac765f9d2042ad5b744bcca48fa56d51f7228
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2022-06-09 10:47:44 +0700

    update module4

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,9 +2,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link href="./4/public/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-<link href="./4/public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
@@ -15,7 +12,6 @@
 <link
     href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,300;0,400;0,500;0,600;0,700;1,200;1,400&display=swap"
     rel="stylesheet">
-
 <?php
 if (!class_exists('lessc')) {
     include './4/public/libs/lessc.inc.php';
