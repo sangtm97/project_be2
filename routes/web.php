1b3644d377ec178e0edca3e1e7757c923beb1aad
Dungtt19 <19211tt0992@mail.tdc.edu.vn>
2022-06-10 17:11:04 +0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Admin\Users\LoginController;
+use \App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\SocialController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('admin/users/login',[LoginController::class,'index'])->name('login'); 
+Route::post('admin/users/login/store',[LoginController::class,'store']); 
 
-Route::get('/', function () {
-    return view('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('admin',[MainController::class,'index'])->name('admin');
+    Route::get('admin/main',[MainController::class,'index']);
 });
+
+Route::get('/getInfo-facebook/{social}', 'SocialController@getInfo');
+
+Route::get('/check-info-facebook/{social}', 'SocialController@checkInfo');
