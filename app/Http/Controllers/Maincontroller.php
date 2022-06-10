19407b7eaceb8e80cd85edd0a4103a6e82130977
TruongHoangHai123 <truonghoanghai.tdc2019@gmail.com>
2022-06-10 20:31:39 +0700

    commit code

--- app/Http/Controllers/Maincontroller.php
+++ app/Http/Controllers/Maincontroller.php
@@ -11,8 +11,9 @@ class MainController extends Controller
     {
         $bestsellers = DB::table('products')->where('endow_id', '=', 1)->orderBy('updated_at', 'DESC')->get();
         $recommendeds = DB::table('products')->where('endow_id', '=', 2)->orderBy('updated_at', 'DESC')->get();
+        $limiteds = DB::table('products')->where('endow_id', '=', 3)->orderBy('updated_at', 'ASC')->get();
         $products = DB::table('products')->get();
-        return view('home', compact('bestsellers', 'recommendeds', 'products'));
+        return view('home', compact('bestsellers', 'recommendeds', 'limiteds', 'products'));
         //return dd($bestsellers);
     }
 
--- resources/views/module-6.blade.php
+++ resources/views/module-6.blade.php
@@ -2,10 +2,11 @@
     <div class="container">
         <div class="row">
             <h1 class="page-header__title ">Limited Time Deals</h1><br>
+            @foreach ($limiteds as $limited )
             <div class="col-md-4">
                 <div class="card-product">
                     <div class="card-product-img">
-                        <img class="card-img-top card-product-front" src="./6/public/images/product-10-1.jpg"
+                        <img class="card-img-top card-product-front" src="./upload/{{$limited->product_image}}"
                             alt="Card image cap">
                     </div>
                     <div class="product-button">
@@ -25,17 +26,18 @@
                     </div>
 
                     <div class="card-content">
-                        <div class="content">
-                            <h2 class="card-product-name">Small Side Table</h2>
+                        <div class="content"style="width: 250px;height: 80px;">
+                        <h2 class="product-title" style="font-size:1.5em; text-align:center;padding-top: 20px;"><a href=""></a>{{$limited->product_name}} </h2>
                             <p class="card-product-price">
-                                <span class="old-price">&pound; 184.63</span>
+                            <span class="old-price">${{$limited->product_price}}</span>
                                 <span class="new-price new-price-active">&pound; 14.17</span>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
+            @endforeach
+            <!-- <div class="col-md-4">
                 <div class="card-product">
                     <div class="card-product-img">
                         <img class="card-img-top card-product-front" src="./6/public/images/anh2.jpg"
@@ -45,14 +47,14 @@
                         <div class="btn-add-to-cart mb-2">
                             <div class="btn-view-icon"><i class="fa-solid fa-unlock"></i></div>
                         </div>
-                        <div class="btn-wishlist mb-2">
+                        <div class="btn-wishlist mb-2"> -->
                             <!-- <span class="btn-wishlist-title">Wishlist</span> -->
-                            <span class="btn-wishlist-icon"><i class="fa-regular fa-heart"></i></span>
+                            <!-- <span class="btn-wishlist-icon"><i class="fa-regular fa-heart"></i></span>
 
                         </div>
-                        <div class="btn-view mb-2">
+                        <div class="btn-view mb-2"> -->
                             <!-- <div class="btn-view-title">Quick View</div> -->
-                            <div class="btn-view-icon"><i class="fa-regular fa-eye"></i></div>
+                            <!-- <div class="btn-view-icon"><i class="fa-regular fa-eye"></i></div>
                         </div>
 
                     </div>
@@ -67,8 +69,8 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
+            </div> -->
+            <!-- <div class="col-md-4">
                 <div class="card-product">
                     <div class="card-product-img">
                         <img class="card-img-top card-product-front" src="./6/public/images/anh3.jpg"
@@ -82,14 +84,14 @@
                         <div class="btn-add-to-cart mb-2">
                             <div class="btn-view-icon"><i class="fa-solid fa-unlock"></i></div>
                         </div>
-                        <div class="btn-wishlist mb-2">
+                        <div class="btn-wishlist mb-2"> -->
                             <!-- <span class="btn-wishlist-title">Wishlist</span> -->
-                            <span class="btn-wishlist-icon"><i class="fa-regular fa-heart"></i></span>
+                            <!-- <span class="btn-wishlist-icon"><i class="fa-regular fa-heart"></i></span>
 
                         </div>
-                        <div class="btn-view mb-2">
+                        <div class="btn-view mb-2"> -->
                             <!-- <div class="btn-view-title">Quick View</div> -->
-                            <div class="btn-view-icon"><i class="fa-regular fa-eye"></i></div>
+                            <!-- <div class="btn-view-icon"><i class="fa-regular fa-eye"></i></div>
                         </div>
 
                     </div>
@@ -102,8 +104,8 @@
                             </p>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div> -->
+            <!-- </div> -->
         </div>
     </div>
 </div>
