fdb15f1a7136a2c286fd12c10c1ffd5f81203db0
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2022-06-10 21:19:51 +0700

    commit code

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -76,13 +76,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./add" class="nav-link">
+                <a href="/admin/protypes/add" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Add New Protype</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="./list" class="nav-link">
+                <a href="/admin/protypes/list" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>List Protype</p>
                 </a>
