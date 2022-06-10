f3f338aa604e9a53cee1b02ca7d4ad1f071a40b8
Dungtt19 <19211tt0992@mail.tdc.edu.vn>
2022-06-10 15:05:13 +0700

    commit source module-16

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('head')
+</head>
+<body>
+    @include('module-1')
+    @include('module-16')
+    <!-- @include('module-1')
+    @include('module-3')
+	@include('module-5')
+    @include('module-8') -->
+</body>
+</html>
---
+++ resources/views/module-16.blade.php
@@ -0,0 +1,132 @@
+<div class="module-16">
+        <div class="header">
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3>Related Products</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="content">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col-md-3 text-center product">
+                        <a href="#" class = "img-related">
+                            <img src="http://localhost:70/16/public/images/fur1501.jpg" class="img-fluid img1" alt="">
+                            <img src="http://localhost:70/16/public/images/fur1502.jpg" class="img-fluid img2" alt="">
+                            <div class="content-an1">
+                                <i class="fa-solid fa-shield-heart"></i>
+                            </div>
+                            <div class="content-an2">
+                                <i class="fa-solid fa-eye"></i>
+                                    
+                            </div>
+                            <div class="add">
+                                <i class="fa-solid fa-lock"></i>
+                                Add to card
+                            </div>
+                        </a>
+                        <div class="div pt-3">
+                            <a href="#">
+                                <h6>
+                                    Urchin Sculpture Small
+                                </h6>
+                            </a>
+                            <p>$320.00</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center product">
+                        <a href="#" class = "img-related">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2020/12/fur1201.jpg?resize=570%2C684&ssl=1" class="img-fluid img1" alt="">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2020/12/fur1202.jpg?resize=570%2C684&ssl=1" class="img-fluid img2" alt="">
+                            <div class="content-an1">
+                            <i class="fa-solid fa-shield-heart"></i>
+                            </div>
+                            <div class="content-an2">
+                                <i class="fa-solid fa-eye"></i>
+                                    
+                            </div>
+                            <div class="add">
+                                <i class="fa-solid fa-lock"></i>
+                                Add to card
+                            </div>
+                        </a>
+                        <div class="div pt-3">
+                            <a href="#">
+                                <h6>
+                                    Whistler Dining Chair
+                                </h6>
+                            </a>
+                            <p>$77.00</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center product">
+                        <a href="#" class = "img-related">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2020/12/fur1601.jpg?resize=570%2C684&ssl=1" class="img-fluid img1" alt="">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2020/12/fur1602.jpg?resize=570%2C684&ssl=1" class="img-fluid img2" alt="">
+                            <div class="content-an1">
+                            <i class="fa-solid fa-shield-heart"></i>
+                            </div>
+                            <div class="banggia1">
+                                HOT
+                            </div>
+                            <div class="banggia2">
+                                -8%
+                            </div>
+                            <div class="content-an2">
+                                <i class="fa-solid fa-eye"></i>
+                                    
+                            </div>
+                            <div class="add">
+                                <i class="fa-solid fa-lock"></i>
+                                Add to card
+                            </div>
+                        </a>
+                        <div class="div pt-3 ">
+                            <a href="#">
+                                <h6>
+                                    Emma Wooden Leg Armchair
+                                </h6>
+                            </a>
+                            <div class="div d-inline-flex">
+                            <s id = "s">$49.00</s>
+                            <p class = "ps-2" id = "p1">$45.00</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-3 text-center product">
+                        <a href="#" class = "img-related">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2021/10/fur1801.jpg?resize=570%2C684&ssl=1" class="img-fluid img1" alt="">
+                            <img src="https://i0.wp.com/demo4.drfuri.com/razzi9/wp-content/uploads/sites/26/2021/10/fur1802.jpg?resize=570%2C684&ssl=1" class="img-fluid img2" alt="">
+                            <div class="content-an1">
+                            <i class="fa-solid fa-shield-heart"></i>
+                            </div>
+                            <div class="banggia1">
+                                HOT
+                            </div>
+                            <div class="content-an2">
+                                <i class="fa-solid fa-eye"></i>
+                                    
+                            </div>
+                            <div class="add-2">
+                                <i class="fa-solid fa-lock"></i>
+                                Select Option
+                            </div>
+                        </a>
+                        <div class="div pt-3 ">
+                            <a href="#">
+                                <h6>
+                                    Light Rattan Laundry Basket
+                                </h6>
+                            </a>
+                            <p class = "ps-2" >$88.62</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
